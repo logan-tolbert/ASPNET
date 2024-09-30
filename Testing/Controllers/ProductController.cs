7090eda59c7b44fc329e72afaeff55037cb27167
logan-tolbert <logo88205+git@gmail.com>
2024-09-30 04:14:46 -0500

    :construction: Add controller

---
+++ Testing/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Testing.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        
+        public IActionResult Index()
+        {
+            var products = _repo.GetAllProducts();
+            return View();
+        }
+    }
+}
